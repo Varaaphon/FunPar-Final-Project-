9f3750d37775675b04d8ac154df0f8130e45165d
Varaaphon <varaaphoncho@outlook.com>
2024-07-19 23:51:50 +0700

    Delete src/test/scala/resources/BenchMark.scala

--- src/test/scala/resources/BenchMark.scala
+++
@@ -1,9 +0,0 @@
-import org.openjdk.jmh.annotations._
-
-@State(Scope.Benchmark)
-class WebScraperBenchmark {
-  @Benchmark
-  def fetchArticlesBenchmark(): Unit = {
-    WebScraper.fetchArticles("https://example.com")
-  }
-}
