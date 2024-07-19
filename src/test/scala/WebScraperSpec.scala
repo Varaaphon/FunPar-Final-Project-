c318ff32b72b803b957540a55f282bf95b420ac0
Varaaphon <varaaphoncho@outlook.com>
2024-07-19 23:11:02 +0700

    Delete src/test/scala/WebScraperSpec.scala

--- src/test/scala/WebScraperSpec.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-
-class WebScraperSpec extends AnyFlatSpec {
-  "WebScraper" should "fetch articles from a given URL" in {
-    val articles = WebScraper.fetchArticles("https://example.com")
-    assert(articles.nonEmpty)
-  }
-}
